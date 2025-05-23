898918cfd62190ae9fa4dff45c220faec45baa9c
Robetjunior <juniorferreirarob@gmail.com>
2025-05-23 14:33:49 -0300

    docs: corrige descrição das funções dos diretórios na estrutura da Clean Architecture

--- src/interfaces/http/validators/user.validator.ts
+++ src/interfaces/http/validators/user.validator.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const createUserSchema = z.object({
-  name: z.string().min(1, 'Name is required'),
+  name: z.string().min(3, 'Name is required'),
   email: z.string().email('Invalid email format'),
 });
 
